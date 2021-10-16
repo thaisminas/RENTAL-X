6a1be69102b25d9b9029f844b3e8cca541b17ac8
thaisminas <thaisminasds@gmail.com>
2021-10-16 11:06:03 -0300

    commit inicial

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express, { request } from 'express';
+
+const app = express()
+
+app.get("/", (request, response) => {
+    return response.json({ message: "hello world"})
+})
+
+app.listen(3333, () => console.log("Server is running!"));
+
